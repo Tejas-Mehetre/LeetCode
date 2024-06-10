7ea448fa271145ad281f61a12b619a89e16dbf9b
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-06-10 20:42:50 +0530

    Time: 4 ms (66.25%), Space: 13.5 MB (39.93%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; // Handle empty input array
-        
-        int index = 0; // Initialize index to keep track of the position for the result
-        
-        for (int num : nums) {
-            // If index is less than 2 or the current number is different from the two elements before it
-            if (index < 2 || num > nums[index - 2]) {
-                nums[index++] = num; // Place the current number at the next available position
+        if(nums.size() == 0) return 0;
+        int index = 0;
+        for(int num : nums){
+            if(index < 2 || nums[index-2] < num){
+                nums[index++] = num;
             }
         }
-        
-        return index; // Return the new length
+        return index;
     }
 };

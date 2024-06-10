dc7b88481a368bc2150ec80fa5b18f02dcc71b1d
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-06-10 13:46:11 +0530

    Time: 0 ms (100%), Space: 10.6 MB (6.28%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ind = 0;
+        for(int num : nums){
+            if(num != val){
+                nums[ind++] = num;
+            }
+        }
+        return ind;
+    }
+};

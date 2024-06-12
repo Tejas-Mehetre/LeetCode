81589664375bb6a482c1444d98a7577ddbd517a9
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-06-12 11:18:05 +0530

    Time: 0 ms (100%), Space: 9.8 MB (71.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,17 +3,16 @@ public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int l = 0, m = 0, h = n-1;
-        while(m <= h){
+        while(m<=h){
             if(nums[m] == 0){
-                swap(nums[l] , nums[m]);
-                l++;
-                m++;
+                swap(nums[m], nums[l]);
+                l++, m++;
             }
             else if(nums[m] == 1){
                 m++;
             }
-            else {
-                swap(nums[m] , nums[h]);
+            else{
+                swap(nums[h], nums[m]);
                 h--;
             }
         }

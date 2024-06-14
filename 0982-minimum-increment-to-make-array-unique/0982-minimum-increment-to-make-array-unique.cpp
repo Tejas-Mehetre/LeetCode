9fd4e4e088a2be4e1d9835a23f3d522d5c63fc25
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-06-14 13:05:37 +0530

    Time: 705 ms (5.11%), Space: 69 MB (93.9%) - LeetHub

---
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int moves = 0;
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                while(nums[i] <= nums[i-1]){
+                    nums[i] += 1;
+                    moves++;
+                }
+            }
+        }
+        return moves;
+    }
+};

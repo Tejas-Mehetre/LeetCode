e00279b40ddf72d62a0a7b87f7e9badae1ce66d9
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-07-02 18:50:33 +0530

    Time: 9 ms (11.77%), Space: 14.4 MB (7.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -3,18 +3,22 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> num;
+        
+        unordered_map<int, int> map;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (nums1[i] == nums2[j]) {
-                    num.push_back(nums2[j]);
-                    nums2[j] = INT_MIN;  
-                    break;  
-                }
+        for(int num : nums1){
+            map[num]++;
+        }
+
+        vector<int> ans;
+
+        for(int num : nums2){
+            if(map[num] != 0){
+                ans.push_back(num);
+                map[num]--;
             }
         }
 
-        return num;
+        return ans;
     }
 };

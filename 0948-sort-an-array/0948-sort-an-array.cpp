c37b76ab0cd760c4da1d625848a1d90828f290e8
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-07-27 21:21:50 +0530

    Time: 434 ms (29.66%), Space: 178.3 MB (26.73%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,48 +1,44 @@
+void merge(vector<int>& array, int low, int mid, int high) {
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    vector<int> left_part(n1), right_part(n2);
+
+    for (int i = 0; i < n1; ++i)
+        left_part[i] = array[low + i];
+    for (int i = 0; i < n2; ++i)
+        right_part[i] = array[mid + 1 + i];
+
+    int p1 = 0, p2 = 0, write_ind = low;
+    while (p1 < n1 && p2 < n2) {
+        if (left_part[p1] <= right_part[p2]) {
+            array[write_ind] = left_part[p1++];
+        } else {
+            array[write_ind] = right_part[p2++];
+        }
+        ++write_ind;
+    }
+
+    while (p1 < n1)
+        array[write_ind++] = left_part[p1++];
+
+    while (p2 < n2)
+        array[write_ind++] = right_part[p2++];
+}
+
+void merge_sort(vector<int>& array, int low, int high) {
+    if (low >= high)
+        return;
+
+    int mid = low + (high - low) / 2;
+    merge_sort(array, low, mid);
+    merge_sort(array, mid + 1, high);
+    merge(array, low, mid, high);
+}
+
 class Solution {
 public:
-    void merge(vector<int>& array, int low, int mid, int high){
-        int n1 = mid - low + 1;
-        int n2 = high - mid;
-
-        vector<int> left(n1), right(n2);
-        for(int i = 0; i<n1; i++){
-            left[i] = array[low+i];
-        }
-        for(int i = 0; i<n2; i++){
-            right[i] = array[mid+1+i];
-        }
-
-        int i = 0, j = 0, k = low;
-        while(i<n1 && j<n2){
-            if(left[i] <= right[j]){
-                array[k] = left[i++];
-            }
-            else{
-                array[k] = right[j++];
-            }
-            k++;
-        }
-
-        while(i<n1){
-            array[k++] = left[i++];
-        }
-
-        while(j<n2){
-            array[k++] = right[j++];
-        }
-    }
-
-    void mergeSort(vector<int>& array, int low, int high){
-        if(low<high){
-            int mid = low + (high-low) / 2;
-            mergeSort(array, low, mid);
-            mergeSort(array, mid+1, high);
-            merge(array, low, mid, high);
-        }
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        merge_sort(nums, 0, nums.size() - 1);
         return nums;
     }
 };

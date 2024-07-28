3503008625e09fb29093977c54513e6a8f130a39
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-07-28 17:10:54 +0530

    Time: 92 ms (77.15%), Space: 70.4 MB (81.16%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int low, int mid, int high){
-        int n1 = mid - low + 1;
-        int n2 = high - mid;
-        vector<int> left(n1), right(n2);
-        for(int i = 0; i<n1; i++){
-            left[i] = nums[low+i];
-        }
-        for(int i = 0; i<n2; i++){
-            right[i] = nums[mid+1+i];
-        }
-        int i = 0, j = 0, k = low;
-        while(i<n1 && j<n2){
-            if(left[i] <= right[j]){
-                nums[k] = left[i++];
-            }
-            else{
-                nums[k] = right[j++];
-            }
-            k++;
-        }
+    // void merge(vector<int>& nums, int low, int mid, int high){
+    //     int n1 = mid - low + 1;
+    //     int n2 = high - mid;
+    //     vector<int> left(n1), right(n2);
+    //     for(int i = 0; i<n1; i++){
+    //         left[i] = nums[low+i];
+    //     }
+    //     for(int i = 0; i<n2; i++){
+    //         right[i] = nums[mid+1+i];
+    //     }
+    //     int i = 0, j = 0, k = low;
+    //     while(i<n1 && j<n2){
+    //         if(left[i] <= right[j]){
+    //             nums[k] = left[i++];
+    //         }
+    //         else{
+    //             nums[k] = right[j++];
+    //         }
+    //         k++;
+    //     }
 
-        while(i<n1){
-            nums[k++] = left[i++]; 
-        }
+    //     while(i<n1){
+    //         nums[k++] = left[i++]; 
+    //     }
 
-        while(j<n2){
-            nums[k++] = right[j++];
-        }
-    }
+    //     while(j<n2){
+    //         nums[k++] = right[j++];
+    //     }
+    // }
 
-    void mergeSort(vector<int>& nums, int low, int high){
-        if(low<high){
-            int mid = low + (high - low) / 2;
-            mergeSort(nums, low, mid);
-            mergeSort(nums, mid+1, high);
-            merge(nums, low, mid, high);
-        }
-    }
+    // void mergeSort(vector<int>& nums, int low, int high){
+    //     if(low<high){
+    //         int mid = low + (high - low) / 2;
+    //         mergeSort(nums, low, mid);
+    //         mergeSort(nums, mid+1, high);
+    //         merge(nums, low, mid, high);
+    //     }
+    // }
 
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        // mergeSort(nums, 0, nums.size()-1);
+        sort(nums.begin(), nums.end());
         return nums;
     }
 };

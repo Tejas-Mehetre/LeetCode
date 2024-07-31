89d73626ce96586142c0a6faae6baaeed048d09e
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-07-31 08:59:13 +0530

    Time: 20 ms (38.98%), Space: 27.1 MB (40.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,13 +3,13 @@ public:
     int majorityElement(vector<int>& nums) {
         int candidate = nums[0];
         int count = 1;
+
         for(int i = 1; i<nums.size(); i++){
-            if(count == 0){
-                candidate = nums[i];
-                count = 1;
-            }
-            else if(candidate == nums[i]){
+            if(candidate == nums[i]){
                 count++;
+            }
+            else if(count == 0){
+                candidate = nums[i];
             }
             else{
                 count--;

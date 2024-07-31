e5c920a736864c6a36304f5e1ee9eb413193c712
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-01 00:35:54 +0530

    Time: 2 ms (38.61%), Space: 7.5 MB (8.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        double num = n;
-        while(num > 0){
-            if(num == 1){
-                return true;
-            }
-            num = num/2;
-        }
-        return false;
+        //***********Approach 1***********
+        // double num = n;
+        // while(num > 0){
+        //     if(num == 1){
+        //         return true;
+        //     }
+        //     num = num/2;
+        // }
+        // return false;
+
+        //***********Approach 2***********
+        return (n > 0 && (n & (n-1)) == 0) ? true : false;
     }
 };

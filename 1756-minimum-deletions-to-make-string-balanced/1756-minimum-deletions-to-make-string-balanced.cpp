0848934f824b27c2335766ad4d84e18488a3afec
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-07-31 08:52:43 +0530

    Time: 105 ms (38.5%), Space: 24.6 MB (51.49%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        int f[n + 1];
-        memset(f, 0, sizeof(f));
-        int b = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (s[i - 1] == 'b') {
-                f[i] = f[i - 1];
-                ++b;
-            } else {
-                f[i] = min(f[i - 1] + 1, b);
+        stack<char> st;
+        int count = 0; 
+
+        for(char c : s){
+                if(!st.empty() && st.top() == 'b' && c == 'a'){
+                    st.pop();
+                    count++;
+                }
+                else{
+                    st.push(c);
+                }
             }
-        }
-        return f[n];
+        return count;
     }
 };

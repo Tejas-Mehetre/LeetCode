e04e36695f65c8c72a148eeba636e4427445fc9b
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-07 23:15:16 +0530

    Time: 204 ms (5.72%), Space: 95.5 MB (5.09%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        for(long a = 0; a*a <= c; a++){
-            long b = c - a*a;
-            double sqrt_b = sqrt(b);
-            if(sqrt_b == (int)sqrt_b){
-                return true;
-            }
+        // for(long a = 0; a*a <= c; a++){
+        //     long b = c - a*a;
+        //     double sqrt_b = sqrt(b);
+        //     if(sqrt_b == (int)sqrt_b){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //Method 2
+        if(c == 0) return true;
+        unordered_map<long long, long long>map;
+        for(long i = 0; i*i<c; i++){
+            map[i*i]++;
+        }
+
+        for(long i = 0; i*i<=c; i++){
+            long long val = c - (i*i);
+            if(map.count(val)) return true;
         }
         return false;
     }

9e19c04545a0e772ec402eaae46230f6dd2cd96d
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-10 00:31:21 +0530

    Time: 69 ms (72.52%), Space: 27.7 MB (21.52%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> letterFreq(26, 0);
-        for (char c : word) {
-            letterFreq[c - 'a']++;
+        vector<int> map(26, 0);
+        for(char c : word){
+            map[c - 'a']++;
         }
-        
-        sort(letterFreq.rbegin(), letterFreq.rend());
-        
+
+        sort(map.rbegin(), map.rend());
+
         int totalPresses = 0;
-        for (int i = 0; i < 26; i++) {
-            if (letterFreq[i] == 0) break;
-            totalPresses += (i / 8 + 1) * letterFreq[i];
+        for(int i = 0; i<26; i++){
+            if(map[i] == 0) break;
+            totalPresses += ((i / 8) + 1) * map[i];
         }
-        
         return totalPresses;
     }
 };

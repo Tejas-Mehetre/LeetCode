d5072a913c55330777a7f0b29086c146f56daf3e
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-11 22:05:45 +0530

    Time: 19 ms (47.08%), Space: 27.3 MB (34.98%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,9 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int n = s.size();
         for(int i = 0, j = n-1; i<j; i++, j--){
-            int temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+            swap(s[i], s[j]);
         }
     }
 };

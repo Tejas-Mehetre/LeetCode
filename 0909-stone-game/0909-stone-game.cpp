7baa1ee45f8738b4248e1e6c290abb28f9e14ef3
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-20 20:41:41 +0530

    Time: 19 ms (16.34%), Space: 18.7 MB (11.49%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(vector<int>& piles, int i, int j, vector<vector<int>>& dp){
+        if(i > j){
+            return 0;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int takeFirst = piles[i] - solve(piles, i + 1, j, dp);
+
+        int takeLast = piles[j] - solve(piles, i, j - 1, dp);
+
+        return dp[i][j] = max(takeFirst, takeLast);
+    }
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(piles, 0, n-1, dp);
+    }
+};

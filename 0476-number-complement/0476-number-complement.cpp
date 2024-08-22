42ad92abbfd633e511c9d06dd182aacedfdce782
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-22 20:57:06 +0530

    Time: 0 ms (100%), Space: 7.6 MB (8.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -3,8 +3,8 @@ public:
     int findComplement(int num) {
         int numOfBits = int(log2(num)) + 1;
 
-        for(int i = 0; i< numOfBits; i++){
-            num = num ^ (1 << i);
+        for(int i = 0; i<numOfBits; i++){
+            num ^= (1 << i);
         }
         return num;
     }

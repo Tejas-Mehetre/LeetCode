787aac46ce4d65c423262150be84cf2d6aab1e86
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-24 21:00:59 +0530

    Time: 69 ms (67.89%), Space: 86.1 MB (12.6%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,42 +1,38 @@
-static const int __ = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
-
-int init = [] {
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string str; getline(cin, str);) {
-        int cnt5 = 0, cnt10 = 0;
-        for (int i = 1, n = str.size(); i < n; i += 2) {
-            int b = (str[i] == '5') ? 5 : 10 * (str[i++] - '0');
-            switch (b) {
-            case 5:
-                ++cnt5;
-                break;
-            case 10:
-                if (!cnt5) { cout << "false\n"; goto end; }
-                --cnt5;
-                ++cnt10;
-                break;
-            default:
-                if (cnt5 && cnt10) --cnt5, --cnt10;
-                else if (cnt5 > 2) cnt5 -= 3;
-                else { cout << "false\n"; goto end; }
-            }
-        }
-        cout << "true\n";
-        end:
-    }
-    exit(0);
-    return 0;
-}();
-
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        return false;
+        int five = 0, ten = 0;
+
+        for(int i = 0; i<bills.size(); i++){
+            if(bills[i] == 5){
+                five++;
+            }
+
+            else if(bills[i] == 10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            else if(bills[i] == 20){
+                if((five > 2) || (five > 0 && ten > 0)){
+                    if(five > 0 && ten > 0){
+                        ten--;
+                        five--;
+                    }
+                    else{
+                        five -= 3;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
